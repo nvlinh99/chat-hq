eba0d385be5f3e11bb2232bb7e17a63270c30c41
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2020-08-13 14:44:17 +0700

    Add db connection

--- server/app.js
+++ server/app.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const errorHandlers = require('./handlers/errorHandlers');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Setup error handlers
+app.use(errorHandlers.catchError);
+app.use(errorHandlers.mongooseErrors);
+if(process.env.NODE_ENV === "DEVELOPMENT") {
+	app.use(errorHandlers.devErrors);
+} else {
+	app.use(errorHandlers.productionErrors);
+}
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || process.env.LOCAL_PORT;
+
+
+mongoose
+.connect(process.env.DB_URL, { 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true,
+    useFindAndModify: false,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
+}); 
 
 app.listen(PORT, () => {console.log(`Server is listening on port ${PORT}`);});
