e725ae25979edd8aeee9be072b2f2014ac83fbe7
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2020-08-15 11:08:52 +0700

    Add auth middleware

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
 const asyncHandler = require('express-async-handler');
 
 const AppError = require('../utils/appError');
@@ -11,45 +10,6 @@ const signToken = (type, id) => {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
-
-exports.authorize = asyncHandler(async (req, res, next) => {
-  // Get token
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer ')
-  ) {
-    token = req.headers.authorization.split(' ')[1];
-  } else {
-    return next(
-      new AppError('You are not logged in! Please log in to get access.', 401)
-    );
-  }
-
-  // Verify token
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-  // Check user exists
-  let currentUser = null;
-  switch (decoded.type) {
-    case 'user':
-      currentUser = await User.findOne({ where: { email: decoded.email } });
-      break;
-    default:
-  }
-
-  if (!currentUser) {
-    return next(
-      new AppError(
-        'The user belonging to this token does no longer exists.',
-        401
-      )
-    );
-  }
-  // GRANT ACCESS
-  req.user = currentUser;
-  next();
-});
 
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.body;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
 const userController = require('../controllers/user.controller');
-const { catchErrors } = require('../handlers/errorHandlers');
+const authMiddleware = require('../middlewares/auth.middleware');
 
+// Public routes
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+
+// Auth route
+router.use(authMiddleware.authorize);
 
 module.exports = router;
