8cc6821583206627354f2eaf1bba7790b4224673
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2020-08-16 08:11:42 +0700

    Client: Delete console.log()

--- client-react/src/components/IndexPage.js
+++ client-react/src/components/IndexPage.js
@@ -3,7 +3,6 @@ import React from 'react'
 const IndexPage = (props) => {
 	React.useEffect(() => {
 		const token = localStorage.getItem("CC_Token");
-		console.log(token);
 		if(!token) {
 			props.history.push("/login");
 		}
